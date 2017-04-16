d1b4b3e54937fd8f32e298a02269b16479ccb8a5
Neil Mather <ngmather@gmail.com>
2017-04-16 19:20:15 +0100

    Setup codeception infrastructure

--- Features/B-Features/B.2-Publishing/1-PublishNote.feature
+++ Features/B-Features/B.2-Publishing/1-PublishNote.feature
@@ -10,8 +10,14 @@ I want to be able to post notes to my website.
 
 ----
 
+A note is a post that is typically short unstructured* plain text, written &
+posted quickly, that has its own permalink page.
+
+See: [indieweb.org/note](indieweb.org/note).
+
 Scenario: Publish note
 
-Given 
-When I publish a note
-Then it is parsed correctly as a note
+Given I am logged in to my site
+When I publish a note with the text 'This is my note!':
+Then it appears correctly as a note
+And it is marked up correctly as a note
---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,58 @@
+<?php
+
+
+/**
+ * Inherited Methods
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+
+    /**
+     * @Given I am logged in to my site
+     */
+    public function iAmLoggedInToMySite()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `I am logged in to my site` is not defined");
+    }
+
+    /**
+     * @When I publish a note with the text 'This is my note!':
+     */
+    public function iPublishANoteWithTheTextThisIsMyNote()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `I publish a note with the text 'This is my note!':` is not defined");
+    }
+
+    /**
+     * @Then it appears correctly as a note
+     */
+    public function itAppearsCorrectlyAsANote()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `it appears correctly as a note` is not defined");
+    }
+
+    /**
+     * @Then it is marked up correctly as a note
+     */
+    public function itIsMarkedUpCorrectlyAsANote()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `it is marked up correctly as a note` is not defined");
+    }
+}
